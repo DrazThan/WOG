16037a39fac210cb169e1b21a4590074630b9f16
DrazThan <oz.tal000@gmail.com>
2024-07-23 07:55:21 +0300

    moved import to start of app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from score import add_score
+from currency_roulette_game import play_currency_roulette
+from memory_game import generate_sequence,get_list_from_user,is_list_equal
+from guess_game import generate_number, get_guess_user, compare_results
+
 
 # This file acts as the main menu and entry point for the games.
 
@@ -37,18 +41,15 @@ def main_menu():
     while True:
         game,user_difficulty_select = start_play()
         if game == "2":
-            from guess_game import generate_number, get_guess_user, compare_results
             random_number = generate_number(user_difficulty_select)
             user_guess = get_guess_user(user_difficulty_select)
             user_wins = compare_results(user_guess, random_number)
 
         elif game == "1":
-            from memory_game import generate_sequence,get_list_from_user,is_list_equal
             random_list = generate_sequence(int(user_difficulty_select))
             user_list = get_list_from_user()    
             user_wins = is_list_equal(user_list,random_list)
         elif game == "3":
-            from currency_roulette_game import play_currency_roulette
             user_wins = play_currency_roulette(user_difficulty_select)
         if user_wins:
             add_score(user_difficulty_select)
